9df6f9cb756b809f6610d53118e36115b4576f0e
FutureYY <wongyy703@gmail.com>
2026-01-29 04:30:31 +0000

    testing for .26 image

--- java-web-app/src/main/java/com/example/demo/DemoApplication.java
+++ java-web-app/src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   public String home() {
-    return "Hello Flux! Are you feeling good today?";
+    return "Hello Flux from GitOps!";
   }
 }
