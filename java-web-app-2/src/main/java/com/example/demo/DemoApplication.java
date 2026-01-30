37614b993ee065617ccdc30b8f9b1286eb59eb99
FutureYY <wongyy703@gmail.com>
2026-01-30 05:05:53 +0000

    test duplicate source code

--- java-web-app-2/src/main/java/com/example/demo/DemoApplication.java
+++ java-web-app-2/src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   public String home() {
-    return "Hello Docker World!"
+    return "Hello Docker World!";
+    return "Hello Docker World!";
   }
 }
